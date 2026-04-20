c73872fe579fb328c311391c0b1c7007f7bc5115
Cesar <cesar.aguilarh@gmail.com>
2026-04-20 15:14:18 -0500

    Initial Selenium framework setup

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import utils.ConfigReader;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void useCanLogin() {
+        driver.get(ConfigReader.getProperty("baseUrl"));
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(
+                ConfigReader.getProperty("username"),
+                ConfigReader.getProperty("password")
+        );
+
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle,"");
+    }
+}
